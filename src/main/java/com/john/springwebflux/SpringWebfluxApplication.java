d4bf2a2980b876756250fe2f8cbc5e6a29affd16
John <jbyepez@gmail.com>
2024-08-22 19:45:11 -0500

    Update

--- src/main/java/com/john/springwebflux/SpringWebfluxApplication.java
+++ src/main/java/com/john/springwebflux/SpringWebfluxApplication.java
@@ -1,14 +1,22 @@
 package com.john.springwebflux;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 public class SpringWebfluxApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringWebfluxApplication.class);
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(SpringWebfluxApplication.class, args);
+
+        logger.info("Tamaño del pool de threads: {}", Schedulers.DEFAULT_BOUNDED_ELASTIC_SIZE);
+		logger.info("Tamaño de la cola de espera del pool de threads: {}", Schedulers.DEFAULT_BOUNDED_ELASTIC_QUEUESIZE);
+		logger.info("Virtual threads: {}", Schedulers.DEFAULT_BOUNDED_ELASTIC_ON_VIRTUAL_THREADS);
 	}
 
 }
